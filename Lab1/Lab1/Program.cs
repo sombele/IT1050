5bda2866f7c04394397b078e27ced8dcd397ef61
sombele <31907389+sombele@users.noreply.github.com>
2017-09-26 20:54:15 -0400

    I added Lab 1
    I added Lab 1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("korndiolle: ");
+            char korndiolle = char.Parse(Console.ReadLine());
+            Console.Write("ndour: ");
+            string ndour = Console.ReadLine();
+
+            string fullName = firstName + " " + korndiolle + ". " + ndour;
+
+            int heightinFeet = 6-03;
+            double heightInFeet = 1.25;
+            double heightInches = 0;
+
+            double totalHeightInches = (heightinFeet * 12) + heightInches;
+            double heightInCM = totalHeightInches * 2.54;
+            Console.WriteLine(fullName);
+            System.Threading.Thread.Sleep(3000);
+            bool isCitizen = true;
+            int age = 25;
+            bool canVote = isCitizen && age >= 21;
+
+            Console.WriteLine(sombelendour);
+            System.Threading.Thread.Sleep(3000);
+        }
+    }
+}

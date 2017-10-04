4dd7292ab2a7fabdc7fcb1fc0b2936f9b72ff9db
sombele <31907389+sombele@users.noreply.github.com>
2017-10-03 20:56:42 -0400

    changes
    changes

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -9,25 +9,23 @@ namespace Lab1
             Console.Write("Please Enter Your First Name: ");
             string firstName = Console.ReadLine();
             Console.Write("Please Enter Your Middle Initial: ");
-            char korndiolle = char.Parse(Console.ReadLine());
-            Console.Write("ndour: ");
-            string ndour = Console.ReadLine();
+            char middlename = char.Parse(Console.ReadLine());
+            Console.Write("please enter your last name: ");
+            string lastname = Console.ReadLine();
+            string fullName = firstName + " " + middlename + ". " + lastname;
 
-            string fullName = firstName + " " + korndiolle + ". " + ndour;
+            Console.Write("How tall are you in feet?");
+            int heightinFeet = int.Parse(Console.ReadLine());
 
-            int heightinFeet = 6-03;
-            double heightInFeet = 1.25;
             double heightInches = 0;
-
             double totalHeightInches = (heightinFeet * 12) + heightInches;
             double heightInCM = totalHeightInches * 2.54;
-            Console.WriteLine(fullName);
-            System.Threading.Thread.Sleep(3000);
+            
             bool isCitizen = true;
             int age = 25;
             bool canVote = isCitizen && age >= 21;
 
-            Console.WriteLine(sombelendour);
+            Console.WriteLine(fullName);
             System.Threading.Thread.Sleep(3000);
         }
     }
